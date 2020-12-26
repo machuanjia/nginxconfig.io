0eff8792dd07e6a357e754fcdc4f3561764a5cb9
machuanjia <yanshi0429@126.com>
2020-12-26 22:46:48 +0800

    add

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = 8080
-app.use("/", express.static(__dirname + "/dist"));
+app.use("/", express.static(__dirname + "/public"));
 app.listen(port, () => console.log(`> Server is running on 127.0.0.1:${port}`));
